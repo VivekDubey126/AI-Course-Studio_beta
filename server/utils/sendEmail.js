4133c2dacc0540f4afd94eeb4179b0a873dc0085
VivekDubey126 <dubeprince024@gmail.com>
2026-03-12 01:05:06 +0530

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,20 +2,26 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, message) => {
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+  try{
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: subject,
-    html: message
-  });
+    const transporter = nodemailer.createTransport({
+      service:"gmail",
+      auth:{
+        user:process.env.EMAIL_USER,
+        pass:process.env.EMAIL_PASS
+      }
+    });
+
+    await transporter.sendMail({
+      from:process.env.EMAIL_USER,
+      to:email,
+      subject:subject,
+      html:message
+    });
+
+  }catch(error){
+    console.log("Email error:",error);
+  }
 
 };
 
